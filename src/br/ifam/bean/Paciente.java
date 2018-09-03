5533e5262774875585a3d1104b129bebf03624f1
Fernando Gurgel <fag.gurgel@gmail.com>
2018-09-03 17:50:52 -0400

    classe bean

---
+++ src/br/ifam/bean/Paciente.java
@@ -0,0 +1,56 @@
+package br.ifam.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Paciente {
+
+	@Id
+	private long numSUS;
+	private String pressao;
+	private double peso;
+	private Usuario usuario;
+		
+	public Paciente(long numSUS, String pressao, double peso, Usuario usuario) {
+		super();
+		this.numSUS = numSUS;
+		this.pressao = pressao;
+		this.peso = peso;
+		this.usuario = usuario;
+	}
+
+	public Paciente(String pressao, double peso) {
+		super();
+		this.pressao = pressao;
+		this.peso = peso;
+	}
+
+	public Paciente() {}
+	
+	
+	public long getNumSUS() {
+		return numSUS;
+	}
+	public void setNumSUS(long numSUS) {
+		this.numSUS = numSUS;
+	}
+	public String getPressao() {
+		return pressao;
+	}
+	public void setPressao(String pressao) {
+		this.pressao = pressao;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	}
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+}
---
+++ src/br/ifam/bean/Vacina.java
@@ -0,0 +1,66 @@
+package br.ifam.bean;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Vacina {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+	private String nome;
+	private String descricao;
+	private String lote;
+	private Date validade;
+	
+	public Vacina(String nome, String descricao, String lote, Date validade) {
+		this.nome = nome;
+		this.descricao = descricao;
+		this.lote = lote;
+		this.validade = validade;
+	}
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getLote() {
+		return lote;
+	}
+
+	public void setLote(String lote) {
+		this.lote = lote;
+	}
+
+	public Date getValidade() {
+		return validade;
+	}
+
+	public void setValidade(Date validade) {
+		this.validade = validade;
+	}
+}
